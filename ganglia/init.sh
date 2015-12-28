1033230acd42328f4c5e99850f1adda1f6f42526
Gayatri Nesarikar <GayatriSN@users.noreply.github.com>
2015-12-28 20:30:43 +0530

    Update init.sh
    changes for ubuntu

--- ganglia/init.sh
+++ ganglia/init.sh
@@ -11,13 +11,15 @@ chown -R nobody:nobody /mnt/ganglia/rrds
 # Install ganglia
 # TODO: Remove this once the AMI has ganglia by default
 
-GANGLIA_PACKAGES="ganglia ganglia-web ganglia-gmond ganglia-gmetad"
+GANGLIA_PACKAGES="ganglia-monitor rrdtool gmetad ganglia-webfrontend"
 
-if ! rpm --quiet -q $GANGLIA_PACKAGES; then
-  yum install -q -y $GANGLIA_PACKAGES;
+dpkg -l $GANGLIA_PACKAGES | grep "Status" > temp.txt
+
+if ! [[ -s temp.txt]]; then
+  sudo apt-get install -q -y $GANGLIA_PACKAGES;
 fi
 for node in $SLAVES $OTHER_MASTERS; do
-  ssh -t -t $SSH_OPTS root@$node "if ! rpm --quiet -q $GANGLIA_PACKAGES; then yum install -q -y $GANGLIA_PACKAGES; fi" & sleep 0.3
+  ssh -t -t $SSH_OPTS root@$node "dpkg -l $GANGLIA_PACKAGES | grep "Status" > temp.txt; if ! [[ -s temp.txt]]; then sudo apt-get install -q -y $GANGLIA_PACKAGES; fi" & sleep 0.3
 done
 wait
 
